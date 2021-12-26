a6fda79cfb71243046fe1aeea8af1a46d078227f
Deep Chirag <dpcg7125601@gmail.com>
2021-12-26 11:53:59 +0530

    removed conflict

--- ifheplapp/__init__.py
+++ ifheplapp/__init__.py
@@ -69,57 +69,8 @@ def qr_generator(card_name, data):
     img.save('{}.png'.format(data["Name"]))
 
 
-<<<<<<< HEAD
-# def health_card_creation(data):
-#     my_image = Image.open(settings.MEDIA_ROOT + "\\Membership\\health.png")
-#     title_font = ImageFont.truetype("arial.ttf", 35)
-#     title_font1 = ImageFont.truetype("arial.ttf", 60)
-#     image_editable = ImageDraw.Draw(my_image)
-#     image_editable.text(
-#         (635, 299), data["Name"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text((635, 352), format_date(
-#         data["DOB"]), (0, 0, 0), font=title_font)
-#     image_editable.text(
-#         (635, 400), data["Father's / Husband's Name"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text((420, 556), format_num(
-#         str(data["Card Number"])), (255, 253, 246), font=title_font1)
-#     my_image.paste((Image.open("{}.png".format(data["Name"]))).resize(
-#         (220, 220), Image.ANTIALIAS), (58, 210))
-#     # path = os.path.join(settings.MEDIA_ROOT + '\\Membership\\{}\\cards'.format(datetime.now().strftime("%d %m %y")))
-#     # if not path:
-#     #     os.mkdir(path)
-#     my_image.save('{}.png'.format(data["Name"]))
-
-
-# def health_card_creation_back(data):
-#     my_image = Image.open(settings.MEDIA_ROOT +
-#                           "\\Membership\\health_1.png")
-#     print(data["Name"])
-#     title_font = ImageFont.truetype("arial.ttf", 35)
-#     image_editable = ImageDraw.Draw(my_image)
-#     image_editable.text(
-#         (510, 100), data["village"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text(
-#         (510, 152), data["po"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text(
-#         (510, 203), data["ps"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text(
-#         (510, 252), data["block"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text(
-#         (510, 300), data["district"].title(), (0, 0, 0), font=title_font)
-#     image_editable.text(
-#         (760, 348), data["pin_code"].title(), (0, 0, 0), font=title_font)
-#     # path = os.path.join(settings.MEDIA_ROOT +
-#     #                     '\\Membership\\{}\\cards'.format(datetime.now().strftime("%d %m %y")))
-#     # if not path:
-#     #     os.mkdir(path)
-#     my_image.save('{}_1.png'.format(data["Name"]))
-def kisan_card_creation(data):
-    my_image = Image.open(settings.MEDIA_ROOT + "\\Membership\\kisan.png")
-=======
 def health_card_creation(data):
     my_image = Image.open(settings.MEDIA_ROOT + "\\Membership\\health.png")
->>>>>>> 251e91c42869b07e385db33b1b44f7ae85631880
     title_font = ImageFont.truetype("arial.ttf", 35)
     title_font1 = ImageFont.truetype("arial.ttf", 60)
     image_editable = ImageDraw.Draw(my_image)
@@ -139,15 +90,9 @@ def health_card_creation(data):
     my_image.save('{}.png'.format(data["Name"]))
 
 
-<<<<<<< HEAD
-def kisan_card_creation_back(data):
-    my_image = Image.open(settings.MEDIA_ROOT +
-                          "\\Membership\\kisan_1.png")
-=======
 def health_card_creation_back(data):
     my_image = Image.open(settings.MEDIA_ROOT +
                           "\\Membership\\health_1.png")
->>>>>>> 251e91c42869b07e385db33b1b44f7ae85631880
     print(data["Name"])
     title_font = ImageFont.truetype("arial.ttf", 35)
     image_editable = ImageDraw.Draw(my_image)
@@ -168,8 +113,6 @@ def health_card_creation_back(data):
     # if not path:
     #     os.mkdir(path)
     my_image.save('{}_1.png'.format(data["Name"]))
-<<<<<<< HEAD
-=======
 def kisan_card_creation(data):
     my_image = Image.open(settings.MEDIA_ROOT + "\\Membership\\kisan.png")
     title_font = ImageFont.truetype("arial.ttf", 35)
@@ -214,7 +157,6 @@ def kisan_card_creation_back(data):
     # if not path:
     #     os.mkdir(path)
     my_image.save('{}_1.png'.format(data["Name"]))
->>>>>>> 251e91c42869b07e385db33b1b44f7ae85631880
 
 # def membership_card_creation(data):
 #     my_image = Image.open(settings.MEDIA_ROOT + "\\Membership\\membership.png")
