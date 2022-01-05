bc610d151a2eac4e1a61675b9c6eeb78127cc6cc
Deep Chirag <deep.chirag@atriina.com>
2022-01-05 23:10:17 +0530

    Filter for cards

--- ifheplapp/admin.py
+++ ifheplapp/admin.py
@@ -40,7 +40,7 @@ class MembershipResource(resources.ModelResource):
 @admin.register(Membership)
 class MembershipAdmin(ImportExportModelAdmin):
     resource_class = MembershipResource
-    list_display = ("name", "card_number", "reference_number","employeename","created",)
+    list_display = ("name", "card_number", "reference_number","submitted_on","employeename","created",)
     fieldsets = (
         ('Employee Details', {
             'fields': (('employeeID', 'employeename'),)
@@ -68,7 +68,7 @@ class KisanCardResource(resources.ModelResource):
 @admin.register(KisanCard)
 class KisanCardAdmin(ImportExportModelAdmin):
     resource_class = KisanCardResource
-    list_display = ("name", "card_number", "reference_number","employeename","created",)
+    list_display = ("name", "card_number", "reference_number","submitted_on","employeename","created",)
     fieldsets = (
         ('Employee Details', {
             'fields': (('employeeID', 'employeename'),)
@@ -97,7 +97,7 @@ class HealthCardResource(resources.ModelResource):
 @admin.register(HealthCard)
 class HealthCardAdmin(ImportExportModelAdmin):
     resource_class = HealthCardResource
-    list_display = ("name", "card_number", "reference_number","employeename","created",)
+    list_display = ("name", "card_number", "reference_number","submitted_on","employeename","created",)
     fieldsets = (
         ('Employee Details', {
             'fields': (('employeeID', 'employeename'),)
