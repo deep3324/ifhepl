9dcd7f08ba4fd121cf5953355ea06ac4b3625aaf
Ubuntu <ubuntu@ip-172-31-23-90.us-east-2.compute.internal>
2022-01-24 22:48:07 +0530

    latest data

--- ifhepl/settings.py
+++ ifhepl/settings.py
@@ -14,10 +14,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib import messages
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -154,15 +155,13 @@ TAGGIT_CASE_INSENSITIVE = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # EMAIL_HOST='smtp.zoho.in'
-EMAIL_HOST='smtp.gmail.com'
-
+EMAIL_HOST=env('EMAIL_HOST')
 EMAIL_USE_TLS=True
 
 EMAIL_PORT= 587
 
-EMAIL_HOST_USER='care@ifhepl.in'
-with open('/etc/email_password.txt') as f:
-    EMAIL_HOST_PASSWORD = f.read().strip()
+EMAIL_HOST_USER=env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASSWORD')
 
 REST_FRAMEWORK = {
      'DEFAULT_PERMISSION_CLASSES': [
