ff4f076cc82238cae496eb158888d80fd3170430
deep chirag <dpcg7125601@gmail.com>
2022-02-19 19:53:14 +0530

    host change

--- ifhepl/settings.py
+++ ifhepl/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["35.154.242.63"]
+ALLOWED_HOSTS = ["ifhepl.in","www.ifhepl.in"]
 
 # Application definition
 
